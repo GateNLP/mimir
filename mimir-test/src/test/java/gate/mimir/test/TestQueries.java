003960cb8c7be5c73e3dd30ea4bb15b633b8c10e
Ian Roberts <i.roberts@dcs.shef.ac.uk>
2019-04-16 22:39:32 +0100

    Version number for Mimir components to 6.1-SNAPSHOT, depending on gate-core/gate-spring 8.6-SNAPSHOT

--- mimir-test/src/test/java/gate/mimir/test/TestQueries.java
+++ mimir-test/src/test/java/gate/mimir/test/TestQueries.java
@@ -89,11 +89,11 @@ public class TestQueries {
   public static void oneTimeSetUp() throws Exception {
     Gate.init();
     // load the tokeniser plugin
-    Gate.getCreoleRegister().registerPlugin(new Plugin.Maven("uk.ac.gate.plugins", "annie", "8.5-SNAPSHOT"));
+    Gate.getCreoleRegister().registerPlugin(new Plugin.Maven("uk.ac.gate.plugins", "annie", "8.6-SNAPSHOT"));
     // load the DB plugin
-    Gate.getCreoleRegister().registerPlugin(new Plugin.Maven("uk.ac.gate.mimir", "mimir-plugin-dbh2", "5.6-SNAPSHOT"));
+    Gate.getCreoleRegister().registerPlugin(new Plugin.Maven("uk.ac.gate.mimir", "mimir-plugin-dbh2", "6.1-SNAPSHOT"));
     // load the measurements plugin
-    Gate.getCreoleRegister().registerPlugin(new Plugin.Maven("uk.ac.gate.mimir", "mimir-plugin-measurements", "5.6-SNAPSHOT"));
+    Gate.getCreoleRegister().registerPlugin(new Plugin.Maven("uk.ac.gate.mimir", "mimir-plugin-measurements", "6.1-SNAPSHOT"));
     
     
     indexDirs = new File[helperTypes.length];
